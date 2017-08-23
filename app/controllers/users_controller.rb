2eb62e431dd529394b2a6095a429de23b15607a4
Malik <mip12793@gmail.com>
2017-08-23 15:45:19 -0400

    pushing updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,33 @@
 class UsersController < ApplicationController
+
+	after_action :create_profile, only: [:create]
+
 	def index
 		@Users = User.all
 	end
 
-	def edit
-		@Users = User.find(params[:id])
-		puts @Users.inspect
-	end
+	# def edit
+	# 	@Users = User.find(params[:id])
+	# 	puts @Users.inspect
+	# end
+	#
+	# def update
+	# 	@Users = User.find(params[:id])
+	# 	@Users.update_attributes(username: params[:user][:username], password: params[:user][:password], email: params[:user][:email])
+	# 	redirect_to '/profile'
+	# end
+	#
+	# def destroy
+	# 	@Users = User.find(params[:id])
+	# 	@users.destroy
+	# 	redirect_to 'index'
+	# end
+	
+	private
 
-	def update
-		@Users = User.find(params[:id])
-		@Users.update_attributes(username: params[:user][:username], password: params[:user][:password], email: params[:user][:email])
-		redirect_to '/profile'
-	end
-
-	def destroy
-		@Users = User.find(params[:id])
-		@users.destroy
-		redirect_to 'index'
+	def create_profile
+		puts params
+		puts "Hello"
 	end
 
 end
