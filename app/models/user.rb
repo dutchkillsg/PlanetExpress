b448c73ae7e4a92ecb7844b1c6c5534981367f9c
Malik <mip12793@gmail.com>
2017-08-23 19:45:53 -0400

    got rid of profiles. fixed login. added ability to edit a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_one :profile
-	has_many :spaceships, through: :profile
+	has_many :jobs
+	has_many :spaceships
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'users#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :profiles
   resources :spaceships
   resources :jobs
   resources :spaceship_jobs
